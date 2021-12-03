6dd227f54c172445e00b1baa805b8cc35eb08c83
PatFagan <patrickfagan1586@gmail.com>
2021-12-03 01:37:20 -0500

    hold to equip/unequip

--- SvalbardGame/Assets/Scripts/Shooting.cs
+++ SvalbardGame/Assets/Scripts/Shooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class Shooting : MonoBehaviour
     public bool gunEquipped = false;
     public bool hammerPulled = false;
 
+    float equipTimer = 0f;
+
+    float EQUIP_TIME = 300f;
+    public Image equipProgress;
+    bool equipTimerLocked = false;
+
     void Start()
     {
         spawnPos = new Vector3(offsetX, offsetY, offsetZ);
@@ -20,10 +27,24 @@ public class Shooting : MonoBehaviour
 
     void Update()
     {
+        equipProgress.fillAmount = equipTimer / EQUIP_TIME;
+
         timer += Time.deltaTime; // timer
 
-        if (Input.GetButtonDown("EquipGun") && hammerPulled == false) // if gun isn't equipped, equip it
-            gunEquipped = !gunEquipped;
+        if (Input.GetButtonDown("EquipGun") || Input.GetButtonUp("EquipGun"))
+            StartCoroutine(EquipTimer());
+
+        if (Input.GetButton("EquipGun") && hammerPulled == false) // if gun isn't equipped, equip it
+        {
+            if (equipTimerLocked == false)
+                equipTimer++;
+
+            if (hammerPulled == false && equipTimer > EQUIP_TIME)
+            {
+                gunEquipped = !gunEquipped;
+                StartCoroutine(EquipTimer());
+            }
+        }
 
         if (Input.GetButtonDown("Interact") && timer >= timeBetweenShots && gunEquipped)
         {
@@ -46,5 +67,13 @@ public class Shooting : MonoBehaviour
         timer = 0;
         hammerPulled = false;
         Destroy(GameObject.FindGameObjectWithTag("GunCursor"));
+    }
+
+    IEnumerator EquipTimer()
+    {
+        equipTimer = 0f;
+        equipTimerLocked = true;
+        yield return new WaitForSeconds(2f);
+        equipTimerLocked = false;
     }
 }
